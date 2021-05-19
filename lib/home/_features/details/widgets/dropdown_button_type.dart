5ddcc3aea7aef5170947bef995801f7af91dcc52
Gene <45580241+Gene-Dana@users.noreply.github.com>
2021-05-19 16:19:39 -0400

    cleanup

--- lib/home/_features/details/widgets/dropdown_button_type.dart
+++ lib/home/_features/details/widgets/dropdown_button_type.dart
@@ -34,8 +34,10 @@ class DropdownButtonType extends StatelessWidget {
             onChanged: (String? newValue) {
               onChange(newValue!);
             },
-            items: ['Multiple Choice', 'True or False', 'Keyword']
-                .map<DropdownMenuItem<String>>((String value) {
+            items: [
+              'Multiple Choice',
+              'True or False',
+            ].map<DropdownMenuItem<String>>((String value) {
               return DropdownMenuItem<String>(
                 value: value,
                 child: Text(value),
